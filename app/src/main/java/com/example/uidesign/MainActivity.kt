c226331ddada841c978e13e93a5d696a6b66ff4c
Mayank Singh Shakya <mayank1807.singh@gmail.com>
2023-03-03 16:02:19 +0530

    shimmer effect

--- app/src/main/java/com/example/uidesign/MainActivity.kt
+++ app/src/main/java/com/example/uidesign/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -14,18 +15,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val mainViewModel : MainViewModel by viewModels()
-
-
-                MainScreen(mainViewModel = mainViewModel)
+           MainScreen()
 
         }
     }
 }
 
-
+@Composable
+fun MainScreen(){
+    Column {
+        repeat(10){
+            AnimatedShimmer()
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-
+    MainScreen()
 }
--- app/src/main/java/com/example/uidesign/SearchWidgetState.kt
+++
@@ -1,6 +0,0 @@
-package com.example.uidesign
-
-enum class SearchWidgetState {
-    OPENED,
-    CLOSED,
-}
